daeb42c661cc71e7bfa4946e835bd91afc7543d5
zhengjiawen <756848513@qq.com>
2016-09-18 22:57:51 +0800

    12

---
+++ gittest/src/main/java/com/example/gittest/Test.java
@@ -0,0 +1,11 @@
+package com.example.gittest;
+
+/**
+ * Created by outi on 2016/9/18.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("12");
+    }
+
+}
